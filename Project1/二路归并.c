9290c9a401cd83ca8eb4253362c8a1be9ed0d38b
可青 王 <1139719961@qq.com>
2018-10-18 19:05:26 +0800

    满二叉树

--- Project1/二路归并.c
+++ Project1/二路归并.c
@@ -1,44 +1,64 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#define Depth 8
-//void merge(int a[], int b)
-//{
-//
-//}
-//void divide(int a[],int b)
-//{
-//	if (b != 2)
-//		divide(a, b / 2);
-//	merge(a, b);
-//}
-//为什么不直接定义二叉树，在二叉树中操作
- struct data
+int Depth;
+typedef struct data
 {
 	int a;
 	struct data* next;
-};
- typedef struct data data;
+}data;
 typedef struct full_binary_tree
 {
-	data *Data;
+	data* Data;
 	int depth;
 	struct full_binary_tree *left, *right, *father;
 }full_binary_tree;
-
-full_binary_tree creat()
+data *creat_data(int i)
 {
-	data *p;
-	int i = 1, n;
-	while (i <= Depth)
+	data *p, *q, *h;
+	int n;
+	n = pow(2, Depth - i);
+	h = p = malloc(sizeof(data));
+	h->a = 10000;
+	for (int m = 0; m < n; m++)
 	{
-		n = pow(2, Depth - i);
-		p = malloc(sizeof(data));
+		q = malloc(sizeof(data));
+		p->next = q;
+		p = p->next;
 	}
+	p->next = h;
+	return h;
 }
+//满二叉树的定义（以三叉链实现）
+full_binary_tree *creat(int i, full_binary_tree *B)
+{
+	full_binary_tree *A;
+	A = malloc(sizeof(full_binary_tree));
+	A->depth = i;
+	A->Data = creat_data(i);		
+	if (i == 1)
+		A->father = NULL;
+	else A->father = B;
+	if (A->depth == Depth)
+	{
+		A->left = A->right = NULL;
+		return A;
+	}
+	A->left = A->right = creat(i + 1,A);
+	return A;
+}
+
 main()
 {
-	full_binary_tree A;
+	full_binary_tree *A;
+	int x;
+	printf("待排序数的个数：");
+	scanf_s("%d", &x);
+	Depth = (int)(log(x) / log(2) + 1);
+	A= malloc(sizeof(full_binary_tree));
+	A = creat(1, A);
+	
+	/*sort(A);*/
     system("pause");
 }
 
